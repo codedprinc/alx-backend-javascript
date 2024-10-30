46710b15f0c62374fbfcc23128d44364647394d5
Matt Nyaseda <131536270+codedprinc@users.noreply.github.com>
2024-10-30 10:53:12 +0300

    Create api.test.js

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,52 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('should return correct status code 200', (done) => {
+    request.get(`${API_URL}/`, (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return correct result', (done) => {
+    request.get(`${API_URL}/`, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('should return correct status code 200 when id is a number', (done) => {
+    request.get(`${API_URL}/cart/12`, (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return correct result when id is a number', (done) => {
+    request.get(`${API_URL}/cart/12`, (error, response, body) => {
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return 404 when id is not a number', (done) => {
+    request.get(`${API_URL}/cart/hello`, (error, response) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('should return 404 when id contains both numbers and letters', (done) => {
+    request.get(`${API_URL}/cart/12hello`, (error, response) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
