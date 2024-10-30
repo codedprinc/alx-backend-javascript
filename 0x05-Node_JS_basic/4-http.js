17ca1c29566e8c8b70790a255be29649b268e70d
Matt Nyaseda <131536270+codedprinc@users.noreply.github.com>
2024-10-30 05:36:21 +0300

    Create 4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+import http from 'http';
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+export default app;
