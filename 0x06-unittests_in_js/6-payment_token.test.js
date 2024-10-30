97c86319bbda1dbcf4104b5587728919760c9f75
Matt Nyaseda <131536270+codedprinc@users.noreply.github.com>
2024-10-30 10:29:32 +0300

    Update 6-payment_token.test.js

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,15 +1,21 @@
-const expect = require("chai").expect;
-const getPaymentTokenFromAPI = require("./6-payment_token");
+const chai = require('chai');
+const expect = chai.expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
 
-describe("getPaymentTokenFromAPI", () => {
-  it("Should return correct token from API", (done) => {
-    getPaymentTokenFromAPI(true).then((data) => {
-      expect(data).to.equal({ data: "Successful response from the API" });
-    });
-    done();
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a resolved promise with successful response when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => {
+        done(error);  // Call done with error if promise rejects
+      });
   });
 
-  it("Should return nothing", () => {
-    expect(getPaymentTokenFromAPI(false)).to.equal(undefined);
-  })
+  it('should return undefined when success is false', () => {
+    const result = getPaymentTokenFromAPI(false);
+    expect(result).to.be.undefined;
+  });
 });
