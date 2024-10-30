3720f7d682f8d1362ca2034a7cac69be1e09eab4
Matt Nyaseda <mattkeneth@gmail.com>
2024-10-30 09:55:18 +0000

    Fix lint issue

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,28 +1,26 @@
 const fs = require('fs').promises;
 
 // Use a default export as per ESLint's preference for single export modules
-const readDatabase = (path) => {
-  return fs.readFile(path, 'utf8')
-    .then((data) => {
-      const lines = data.split('\n');
-      const studentsData = lines.slice(1).filter((line) => line);
-      const studentsByField = {};
+const readDatabase = (path) => fs.readFile(path, 'utf8')
+  .then((data) => {
+    const lines = data.split('\n');
+    const studentsData = lines.slice(1).filter((line) => line);
+    const studentsByField = {};
 
-      studentsData.forEach((student) => {
-        const [firstName, , , field] = student.split(',');
+    studentsData.forEach((student) => {
+      const [firstName, , , field] = student.split(',');
 
-        if (!studentsByField[field]) {
-          studentsByField[field] = [];
-        }
+      if (!studentsByField[field]) {
+        studentsByField[field] = [];
+      }
 
-        studentsByField[field].push(firstName);
-      });
-
-      return studentsByField;
-    })
-    .catch((error) => {
-      throw error;
+      studentsByField[field].push(firstName);
     });
-};
+
+    return studentsByField;
+  })
+  .catch((error) => {
+    throw error;
+  });
 
 export default readDatabase;
