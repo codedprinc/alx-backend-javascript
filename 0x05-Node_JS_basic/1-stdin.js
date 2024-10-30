fd2219e3167b1b4b55d169b2742e746742969078
Matt Nyaseda <131536270+codedprinc@users.noreply.github.com>
2024-10-30 11:22:45 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,13 @@
-process.stdin.setEncoding('utf-8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  
+  if (name) {
+    process.stdout.write(`Your name is: ${name.toString()}`);
+  }
+});
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    console.log('Your name is:', data.trim().replace("\r", ""));
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    console.log('Your name is:', data.trim().replace("\r", ""));
-  });
-
-  process.stdin.on('end', () => {
-    console.log('This important software is now closing');
-  });
-}
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
