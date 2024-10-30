e8874c281b3464bde7e380ddb25b1afddcca693e
Matt Nyaseda <131536270+codedprinc@users.noreply.github.com>
2024-10-30 13:26:09 +0300

    Update utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,25 +1,27 @@
 const fs = require('fs').promises;
 
-export const readDatabase = (path) => new Promise(async (resolve, reject) => {
-  try {
-    const data = await fs.readFile(path, 'utf8');
+const readDatabase = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8')
+      .then((data) => {
+        const lines = data.split('\n');
+        const studentsData = lines.slice(1).filter((line) => line);
+        const studentsByField = {};
+        
+        studentsData.forEach((student) => {
+          const [firstName, , , field] = student.split(',');
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+          studentsByField[field].push(firstName);
+        });
+        
+        resolve(studentsByField);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
 
-    const lines = data.split('\n');
-
-    const studentsData = lines.slice(1).filter((line) => line);
-    const studentsByField = {};
-
-    studentsData.forEach((student) => {
-      const [firstName, , , field] = student.split(',');
-
-      if (!studentsByField[field]) {
-        studentsByField[field] = [];
-      }
-      studentsByField[field].push(firstName);
-    });
-
-    resolve(studentsByField);
-  } catch (error) {
-    reject(error);
-  }
-});
+export default readDatabase;
